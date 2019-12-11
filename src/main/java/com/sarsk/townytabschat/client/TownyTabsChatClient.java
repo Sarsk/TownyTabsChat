34c94eae064ccdab08f6dc2988bcda46facda583
Sarsk <Sarsk@example.com>
2019-12-11 09:14:26 -0700

    Describe classes
    Hopefully now I wont forget what I'm doing when I put this down for 2 months
    Cleaned up hookIntoChat call

--- src/main/java/com/sarsk/townytabschat/client/TownyTabsChatClient.java
+++ src/main/java/com/sarsk/townytabschat/client/TownyTabsChatClient.java
@@ -12,6 +12,18 @@ import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 
+/*
+ * Class descriptions:
+ *   TownyTabsChatClient - Top level class for Client side logic. Sets up project components and hooks
+ *   GuiNotif - Test class to draw text to the screen
+ *   TabsChatGui - Chat Gui extension
+ *     The Minecraft ChatGui is the text box that lists chat messages and a scroll bar
+ *     Add Tabs for various chat channels and private messages
+ *     Alter contents on the fly to display only the selected channel messages
+ *   TabsChatScreen - Chat Screen extension
+ *     The Minecraft ChatScreen is the text box that accepts user input to send a chat msg
+ *     Extend the Screen to intercept the user text, and add chat channel prefix'
+ */
 public class TownyTabsChatClient
 {
 	static TabsChatGui tabsChatGui;
@@ -24,11 +36,7 @@ public class TownyTabsChatClient
 
 		// Replace the Minecraft Chat GUI
 		tabsChatGui = new TabsChatGui(Minecraft.getInstance());
-		try {
-			hookIntoChat(Minecraft.getInstance().ingameGUI, tabsChatGui);
-		} catch (Exception e) {
-			System.out.println("Error: Unable to hook into chat.  This is bad. Exception: " + e.getMessage());
-		}
+		hookIntoChat(Minecraft.getInstance().ingameGUI, tabsChatGui);
 	}
 
 	@SubscribeEvent
@@ -38,33 +46,37 @@ public class TownyTabsChatClient
 		if (event.getType() != RenderGameOverlayEvent.ElementType.EXPERIENCE) return;
 
 		// TODO: GuiNotif only when needed
-		//new GuiNotif(Minecraft.getInstance());
+		new GuiNotif(Minecraft.getInstance());
 	}
 
 	@SuppressWarnings("MixinClassReference")
-	private static void hookIntoChat(IngameGui guiIngame, TabsChatGui tabsChatGui) throws Exception {
+	private static void hookIntoChat(IngameGui guiIngame, TabsChatGui tabsChatGui) {
 
 		//TODO: TabbyChat had this comparison backwards. Let them know?
 		//TODO: What am I missing in my accesstransformer.cfg? Would rather use AT vs reflection
 
-		if (guiIngame.getChatGUI().getClass().isAssignableFrom(tabsChatGui.getClass())) {
-			// Use reflection, to replace the private final NewChatGui persistantChatGUI with
-			// our own TabsChatGui instance
+		try {
+			if (guiIngame.getChatGUI().getClass().isAssignableFrom(tabsChatGui.getClass())) {
+				// Use reflection, to replace the private final NewChatGui persistantChatGUI with
+				// our own TabsChatGui instance
 
-			// Locate the persistantChatGUI field
-			Field privatePersistantChatGUIField = net.minecraft.client.gui.IngameGui.class.getDeclaredField("persistantChatGUI");
-			privatePersistantChatGUIField.setAccessible(true);
+				// Locate the persistantChatGUI field
+				Field privatePersistantChatGUIField = net.minecraft.client.gui.IngameGui.class.getDeclaredField("persistantChatGUI");
+				privatePersistantChatGUIField.setAccessible(true);
 
-			// Remove the 'final' modifier
-			Field modifiersField = Field.class.getDeclaredField("modifiers");
-			modifiersField.setAccessible(true);
-			modifiersField.setInt(modifiersField, modifiersField.getModifiers() & ~Modifier.FINAL);
+				// Remove the 'final' modifier
+				Field modifiersField = Field.class.getDeclaredField("modifiers");
+				modifiersField.setAccessible(true);
+				modifiersField.setInt(modifiersField, modifiersField.getModifiers() & ~Modifier.FINAL);
 
-			// Apply the change
-			privatePersistantChatGUIField.set(guiIngame, tabsChatGui);
-			System.out.println("Successfully hooked into chat.");
-		} else {
-			System.out.println("Error: We failed to make our chat a child of NewChatGui");
+				// Apply the change
+				privatePersistantChatGUIField.set(guiIngame, tabsChatGui);
+				System.out.println("Successfully hooked into chat.");
+			} else {
+				System.out.println("Error: We failed to make our chat a child of NewChatGui");
+			}
+		} catch (Exception e) {
+			System.out.println("Error: Unable to hook into chat.  This is bad. Exception: " + e.getMessage());
 		}
 	}
 }
